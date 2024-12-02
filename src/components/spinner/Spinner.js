bb684358e9b096491760e42f648495ea9f336051
Adnan Bacic <adnan-bacic@hotmail.com>
2024-12-02 19:56:30 +0100

    bootstrap spinner

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import SpinnerGif from '../../assets/gifs/spinner.gif';
 
 const Spinner = () => {
   return (
-    <div className="text-center">
-      <img src={SpinnerGif} alt="spinner" />
+    <div className="d-flex justify-content-center">
+      <div className="spinner-border" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
   );
 };
